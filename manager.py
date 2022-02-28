7ad7b6f78e92f713ee8e8b088a9de4247ada22a9
FernandoLuiz-web <luizfernandogo.oliveira@outlook.com>
2022-02-28 18:12:27 -0300

    Alteração em classe view no manager.py

--- manager.py
+++ manager.py
@@ -18,7 +18,26 @@ def rna_constructor(input):
     return cont
 
 
-class Dna_Analizer_Controller:
+class View_Manager:
+
+    @staticmethod
+    def HtmlGenerator(name, input):
+        multiplying_operator = 1
+        html = name + ".html"
+        html_path = "static/"+html
+        output = open(html_path, "w")
+        for y in input:
+            transparency = input[y] / max(input.values())
+            output.write("<div style='width:100px; border:1px solid #111; height:100px;"
+                         " float:left; background-color:rgba(0, 0, 0, " + str(transparency) + "')></div>")
+            if multiplying_operator % 4 == 0:
+                output.write("<div style='clear:both'></div>")
+            multiplying_operator += 1
+
+        output.close()
+
+
+class Dna_Analizer_Manager:
     # if a new .fasta file is inserted in the database, it will be implemented only and exclusively in the
     # Dna_Analizer_Controller class!
 
